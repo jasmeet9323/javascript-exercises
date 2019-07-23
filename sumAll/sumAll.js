0c9fed30b343ecda6648ae06bbfb3ad3fa65f46b
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-23 11:33:42 -0700

    Complete sumAll excercise

--- sumAll/sumAll.js
+++ sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+  let min = args[0];
+  let max = args[1];
 
+  if(min < 0 || max < 0 || typeof min !== "number" ||
+    typeof max !== "number")
+      return 'ERROR'
+
+  let sum = 0;
+  
+  if(min > max) {
+    min = min + max;
+    max = min - max;
+    min = min - max;
+  }
+
+  for(let i = min; i <= max; i++) {
+    sum += i;
+  }
+  return sum;
 }
 
 module.exports = sumAll
