0f5a4ad52f5c76c1588b6fea2dc03871c68a155d
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-23 10:34:48 -0700

    Complete removeFromArray excercise.

--- removeFromArray/removeFromArray.js
+++ removeFromArray/removeFromArray.js
@@ -1,5 +1,25 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+  const array = args[0]; // get the array
+  const newArray = []; // return array
 
+  // add to newArray if the item in array is
+  // not in the arguments list
+  array.forEach((item) => {
+    if(!args.includes(item))
+      newArray.push(item);
+  })
+  return newArray;
 }
 
-module.exports = removeFromArray
+module.exports = removeFromArray;
+
+// // A simpler, but more advanced way to do it is to use the 'filter' function,
+// // which basically does what we did with the forEach above.
+
+// // var removeFromArray = function(...args) {
+// //   const array = args[0]
+// //   return array.filter(val => !args.includes(val))
+// // }
+// //
+
+
