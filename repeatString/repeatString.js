6cee2c30d4d4acfbe562b026028c64dfced643f2
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-22 18:11:26 -0700

    Complete repeatString excercise

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+	let rString = '';
+	if(number < 0) {
+    return 'ERROR';
+  } else {
+    for (let i = number - 1; i >= 0; i--) {
+     rString += string;
+   }
+ }
+ return rString;
 }
 
-module.exports = repeatString
+module.exports = repeatString;
