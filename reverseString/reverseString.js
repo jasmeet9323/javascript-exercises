41872f49af5f4423030af52c34ff41cea1e565c4
Jasmeet Singh <jasmeet.singh.mec11@iitbhu.ac.in>
2019-07-22 19:01:44 -0700

    Complete reverseString excercise.

--- reverseString/reverseString.js
+++ reverseString/reverseString.js
@@ -1,5 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+// iterating through string
+//const len = string.length;
+// let rString = '';
+// for(let i = len-1; i >= 0 ;i--){
+//   rString += string[i];
+// }
+// return rString;
+
+// or simply
+ return string.split('').reverse().join('');
 }
 
 module.exports = reverseString
